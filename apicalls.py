b6df5cb54b9aff53f231b80c60e7e703afb524b6
rienvl <mbvanleeuwen2003@yahoo.com>
2024-06-04 08:20:48 +0200

    first implementation of step 5

--- apicalls.py
+++ apicalls.py
@@ -1,20 +1,32 @@
 import requests
-
-#Specify a URL that resolves to your workspace
-URL = "http://127.0.0.1/"
-
+import os
+import json
 
 
-#Call each API endpoint and store the responses
-response1 = #put an API call here
-response2 = #put an API call here
-response3 = #put an API call here
-response4 = #put an API call here
+# Specify a URL that resolves to your workspace
+#URL = 'http://127.0.0.1:8000'
+#URL = 'http://0.0.0.0:8000'
 
-#combine all API responses
-responses = #combine reponses here
+with open('config.json','r') as f:
+    config = json.load(f)
 
-#write the responses to your workspace
+output_path = os.path.join(config['output_model_path'])
 
 
+# Call each API endpoint and store the responses
+response1 = requests.get('http://127.0.0.1:8000/prediction?filename=/testdata/testdata.csv').content  # returns list of predictions
+response2 = requests.get('http://localhost:8000/scoring').content  # returns f1_score
+response3 = requests.get('http://127.0.0.1:8000/summarystats').content  # returns percentage_of_na for each num. column
+response4 = requests.get('http://127.0.0.1:8000/diagnostics').content  # returns timings, percentage_of_na, outdated
+print(response1)
+print(response2)
+print(response3)
+print(response4)
 
+# combine all API responses
+#responses =  # combine responses here
+
+# write the responses to your workspace
+#full_output_path = os.path.join(output_path, 'apireturns.txt')
+#with open(full_output_path, "w") as text_file:
+#    text_file.write("%s" % responses)
--- app.py
+++ app.py
@@ -3,48 +3,82 @@ import pandas as pd
 import numpy as np
 import pickle
 import create_prediction_model
-import diagnosis 
+import diagnosis
+import diagnostics
+import scoring
 import predict_exited_from_saved_model
 import json
 import os
 
 
-
-######################Set up variables for use in our script
-app = Flask(__name__)
+# Set up variables for use in our script
+app = Flask(__name__)  # instantiate app
 app.secret_key = '1652d576-484a-49fd-913a-6879acfa6ba4'
 
-with open('config.json','r') as f:
+with open('config.json', 'r') as f:
     config = json.load(f) 
 
-dataset_csv_path = os.path.join(config['output_folder_path']) 
+dataset_csv_path = os.path.join(config['output_folder_path'])
+full_model_path = os.path.join(config['prod_deployment_path'], 'trainedmodel.pkl')
 
 prediction_model = None
 
 
-#######################Prediction Endpoint
+def read_pandas(filename):
+    df = pd.read_csv(filename)
+    return df
+
+
+# Prediction Endpoint
 @app.route("/prediction", methods=['POST','OPTIONS'])
-def predict():        
-    #call the prediction function you created in Step 3
-    return #add return value for prediction outputs
+def predict():
+    filename = request.args.get('filename')
+    data_df = read_pandas(filename)
+    # call the prediction function you created in Step 3
+    predictions_list = diagnostics.model_predictions(data_df)
 
-#######################Scoring Endpoint
+    return predictions_list, 200  # add return value for prediction outputs
+
+
+# Scoring Endpoint
 @app.route("/scoring", methods=['GET','OPTIONS'])
-def stats():        
-    #check the score of the deployed model
-    return #add return value (a single F1 score number)
+def stats():
+    # load deployed model from model_path specified in config
+    with open(full_model_path, 'rb') as file:
+        model = pickle.load(file)
 
-#######################Summary Statistics Endpoint
+    # check the score of the deployed model on the test data specified in \testdata\testdata.csv
+    f1_score = scoring.score_model(model)
+    return f1_score, 200  # add return value (a single F1 score number)
+
+
+# Summary Statistics Endpoint
 @app.route("/summarystats", methods=['GET','OPTIONS'])
 def stats():        
-    #check means, medians, and modes for each column
-    return #return a list of all calculated summary statistics
+    # check means, medians, and modes for each column
+    statistics_list = diagnostics.dataframe_summary()
 
-#######################Diagnostics Endpoint
+    return statistics_list, 200  # return a list of all calculated summary statistics
+
+
+# Diagnostics Endpoint
 @app.route("/diagnostics", methods=['GET','OPTIONS'])
-def stats():        
-    #check timing and percent NA values
-    return #add return value for all diagnostics
+def stats():
+    """
+    check timing, percentage of NA values, and dependencies
+    :return: timings, percentage_of_na, outdated
+    """
+    # check timing
+    timings = diagnostics.execution_time()
+
+    # check percent NA values
+    percentage_of_na = diagnostics.missing_data()
+
+    # check dependencies
+    outdated = diagnostics.outdated_packages_list()
+
+    return timings, percentage_of_na, outdated, 200  # add return value for all diagnostics
+
 
 if __name__ == "__main__":    
     app.run(host='0.0.0.0', port=8000, debug=True, threaded=True)
