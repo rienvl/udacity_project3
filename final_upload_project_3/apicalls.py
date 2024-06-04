d1f2a5ba0400b4d853ee1fff1e203026f933c057
rienvl <mbvanleeuwen2003@yahoo.com>
2024-06-04 23:38:44 +0200

    added missing file in final_upload directory

--- final_upload_project_3/apicalls.py
+++ final_upload_project_3/apicalls.py
@@ -4,8 +4,8 @@ import json
 
 
 # Specify a URL that resolves to your workspace
-# URL = 'http://127.0.0.1'
-URL = 'http://0.0.0.0'
+URL = 'http://127.0.0.1'
+# URL = 'http://0.0.0.0'
 
 with open('config.json','r') as f:
     config = json.load(f)
@@ -13,8 +13,8 @@ with open('config.json','r') as f:
 output_path = os.path.join(config['output_model_path'])
 
 # Call each API endpoint and store the responses
-# response1 = requests.post(URL + ':8000/prediction?filename=./testdata/testdata.csv').content  # returns list predictions
-response1 = requests.post(URL + ':8000/prediction?filename=~/git/udacity_project3/testdata/testdata.csv').content  # returns list predictions
+response1 = requests.post(URL + ':8000/prediction?filename=./testdata/testdata.csv').content  # returns list predictions
+# response1 = requests.post(URL + ':8000/prediction?filename=~/git/udacity_project3/testdata/testdata.csv').content  # returns list predictions
 response2 = requests.get(URL + ':8000/scoring').content  # returns f1_score
 response3 = requests.get(URL + ':8000/summarystats').content  # returns percentage_of_na for each num. column
 response4 = requests.get(URL + ':8000/diagnostics').content  # returns timings, percentage_of_na, outdated
